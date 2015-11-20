9a1681a14e0eb4a6cb5b99ef608f567ac8a73a38
naineshdesai <nainesh.desai@mutualmobile.com>
2015-11-20 12:32:45 -0600

    Added agent availability feature

--- app/src/main/java/com/humanify/expertconnect/sample/SampleActivity.java
+++ app/src/main/java/com/humanify/expertconnect/sample/SampleActivity.java
@@ -1,7 +1,10 @@
 package com.humanify.expertconnect.sample;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.Toast;
@@ -12,8 +15,12 @@ import com.humanify.expertconnect.ExpertConnect;
 import com.humanify.expertconnect.api.ApiBroadcastReceiver;
 import com.humanify.expertconnect.api.ApiException;
 import com.humanify.expertconnect.api.ExpertConnectApiProxy;
+import com.humanify.expertconnect.api.model.AgentAvailabilityResponse;
+import com.humanify.expertconnect.api.model.ExpertConnectNotification;
 import com.humanify.expertconnect.api.model.ParcelableMap;
+import com.humanify.expertconnect.api.model.SkillStatus;
 import com.humanify.expertconnect.sample.holdr.Holdr_ActivitySample;
+import com.humanify.expertconnect.util.ApiResult;
 import com.humanify.expertconnect.view.compat.MaterialButton;
 
 public class SampleActivity extends AppCompatActivity implements Holdr_ActivitySample.Listener {
@@ -46,6 +53,55 @@ public class SampleActivity extends AppCompatActivity implements Holdr_ActivityS
         }
     };
 
+    ApiBroadcastReceiver<ExpertConnectNotification> notificationReceiver = new ApiBroadcastReceiver<ExpertConnectNotification>() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            super.onReceive(context, intent);
+        }
+
+        @Override
+        public void onSuccess(Context context, ExpertConnectNotification result) {
+            handleAllNotifications(result);
+        }
+
+        @Override
+        public void onError(Context context, ApiException error) {
+
+        }
+    };
+
+    LoaderManager.LoaderCallbacks<ApiResult<AgentAvailabilityResponse>> agentAvailabilityLoader = new LoaderManager.LoaderCallbacks<ApiResult<AgentAvailabilityResponse>>() {
+        private String currentSkill;
+
+        @Override
+        public Loader<ApiResult<AgentAvailabilityResponse>> onCreateLoader(int id, Bundle args) {
+            currentSkill = args.getString("skill");
+            return api.getAgentAvilabilityOnSkill(currentSkill);
+        }
+
+        @Override
+        public void onLoadFinished(Loader<ApiResult<AgentAvailabilityResponse>> loader, ApiResult<AgentAvailabilityResponse> data) {
+            AgentAvailabilityResponse agentAvailResp = data.get();
+            String message = "No Agents available for "+currentSkill;
+
+            if(agentAvailResp.getCount()>0){
+                for (SkillStatus skillStatus : agentAvailResp.getSkillsList()) {
+                    if (skillStatus != null && skillStatus.getSkillName().equals(currentSkill)) {
+                        int agentCount = skillStatus.getAgentsLoggedOn();
+                        message = skillStatus.getSkillName()+" - "+ agentCount
+                                + (agentCount > 1 ? " agents are " : " agent is ") +(skillStatus.isOpen()?"open":"close");
+                    }
+                }
+            }
+            Toast.makeText(SampleActivity.this, message, Toast.LENGTH_LONG).show();
+        }
+
+        @Override
+        public void onLoaderReset(Loader<ApiResult<AgentAvailabilityResponse>> loader) {
+
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -62,6 +118,10 @@ public class SampleActivity extends AppCompatActivity implements Holdr_ActivityS
 
         setSupportActionBar(holdr.toolbar);
         getSupportActionBar().setTitle(getString(R.string.humanify));
+
+        Bundle args = new Bundle();
+        args.putString("skill", DEMO_SKILL);
+        getSupportLoaderManager().initLoader(1, args, agentAvailabilityLoader);
     }
 
     @Override
@@ -70,6 +130,8 @@ public class SampleActivity extends AppCompatActivity implements Holdr_ActivityS
 
         // register decision receiver
         api.registerPostDecisionData(decisionReceiver);
+
+        api.registerForSDKNotifications(notificationReceiver);
     }
 
     @Override
@@ -78,6 +140,7 @@ public class SampleActivity extends AppCompatActivity implements Holdr_ActivityS
 
         // unregister decision receiver
         api.unregister(decisionReceiver);
+        api.unregister(notificationReceiver);
     }
 
     @Override
@@ -113,5 +176,38 @@ public class SampleActivity extends AppCompatActivity implements Holdr_ActivityS
         decisionDict.put("eventId", "sayHello");
 
         api.postDecisionData(decisionDict);
+    }
+
+    private void handleAllNotifications(ExpertConnectNotification notification) {
+        switch (notification.getType()){
+            case ExpertConnectNotification.TYPE_AGENT_AVAILABILITY_INFO_UPDATED:
+                handleAgentAvailabilityNotification(notification);
+                break;
+            case ExpertConnectNotification.TYPE_CHAT_ENDED:
+                handleChatEnd(notification);
+                break;
+            case ExpertConnectNotification.TYPE_CALLBACK_ENDED:
+                handleCallbackEnd(notification);
+                break;
+        }
+    }
+
+    private void handleCallbackEnd(ExpertConnectNotification notification) {
+        String message = "Callback ended with reason - "+notification.getMessage();
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+    }
+
+    private void handleChatEnd(ExpertConnectNotification notification) {
+        String message = "Chat ended with reason - "+notification.getMessage();
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+    }
+
+    private void handleAgentAvailabilityNotification(ExpertConnectNotification notification) {
+        String skill = notification.getMessage();
+        final ExpertConnectApiProxy api = ExpertConnectApiProxy.getInstance(this);
+
+        Bundle args = new Bundle();
+        args.putString("skill", skill);
+        Toast.makeText(this, skill, Toast.LENGTH_LONG).show();
     }
 }
